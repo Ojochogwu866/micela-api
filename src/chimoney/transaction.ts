fa768ca1c8c67a83f327b8c4c921d72181087522
Ojochogwu Dickson <07afrik@gmail.com>
2024-07-21 12:24:17 +0100

    chore: updates

--- src/chimoney/transaction.ts
+++ src/chimoney/transaction.ts
@@ -71,7 +71,7 @@ const sendEmailNotification = async (email: string, amount: number) => {
             to: email,
             subject: 'Payment Received',
             html: `<p>You have received a payment of ${amount} USD.</p>
-                   <p>To cash out the money, please visit the following URL: <a href="https://chimoney-fullstack.vercel.app/">Cash Out</a></p>`
+                <p>To cash out the money, please visit the following URL: <a href="https://chimoney-fullstack.vercel.app/">Cash Out</a></p>`
         };
 
         await transporter.sendMail(mailOptions);
